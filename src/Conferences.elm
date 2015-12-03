9747e0504a5e15d09367d6aab22ba177871d7874
Rob Looby <robertjlooby@gmail.com>
2015-12-02 21:01:27 -0600

    Break out Conferences module

---
+++ src/Conferences.elm
@@ -0,0 +1,41 @@
+module Conferences where
+
+import Date
+import DateFormatter exposing (parseDate)
+
+type alias Model =
+  { conferences : List Conference
+  }
+
+type alias Conference =
+  { name : String
+  , link : String
+  , startDate : Date.Date
+  , endDate : Date.Date
+  , location : String
+  , tags : List Tag
+  }
+
+type Tag =
+  Ruby
+
+list : Model
+list =
+  { conferences =
+      [
+        { name = "NDC London Workshops"
+        , link = "http://ndc-london.com/"
+        , startDate = parseDate "1/11/2016"
+        , endDate = parseDate "1/12/2016"
+        , location = "London, UK"
+        , tags = []
+        }
+      , { name = "NDC London"
+        , link = "http://ndc-london.com/"
+        , startDate = parseDate "1/13/2016"
+        , endDate = parseDate "1/15/2016"
+        , location = "London, UK"
+        , tags = []
+        }
+      ]
+  }
--- src/Main.elm
+++ src/Main.elm
@@ -1,25 +1,7 @@
-module Confs where
-
+import Conferences exposing (Conference, Model, Tag)
+import DateFormatter
 import Html exposing (text)
 import Html.Attributes exposing (href)
-import Date
-import DateFormatter exposing (parseDate)
-
-type alias Model =
-  { conferences : List Conference
-  }
-
-type alias Conference =
-  { name : String
-  , link : String
-  , startDate : Date.Date
-  , endDate : Date.Date
-  , location : String
-  , tags : List Tag
-  }
-
-type Tag =
-  Ruby
 
 view : Model -> Html.Html
 view model =
@@ -50,25 +32,4 @@ conferenceView conference =
 
 main : Html.Html
 main =
-  view initialState
-
-initialState : Model
-initialState =
-  { conferences =
-      [
-        { name = "NDC London Workshops"
-        , link = "http://ndc-london.com/"
-        , startDate = parseDate "1/11/2016"
-        , endDate = parseDate "1/12/2016"
-        , location = "London, UK"
-        , tags = []
-        }
-      , { name = "NDC London"
-        , link = "http://ndc-london.com/"
-        , startDate = parseDate "1/13/2016"
-        , endDate = parseDate "1/15/2016"
-        , location = "London, UK"
-        , tags = []
-        }
-      ]
-  }
+  view Conferences.list
