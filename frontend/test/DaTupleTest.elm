e62db283a17d21f43b5ce148325cff70b6c9423b
Rob Looby <robertjlooby@gmail.com>
2016-10-03 16:32:37 -0500

    Update to elm-test 2.1
    - Leave tests that would require Fuzz.filter commented out

--- frontend/test/DaTupleTest.elm
+++ frontend/test/DaTupleTest.elm
@@ -1,122 +1,111 @@
 module DaTupleTest exposing (..)
 
-import Check exposing (claim, for, is, suite, that)
-import Check.Test
-import Date exposing (Month(..))
-import Date.Extra.Core exposing (monthToInt)
+--import Date exposing (Month(..))
+--import Date.Extra.Core exposing (monthToInt)
+
 import DaTuple exposing (..)
-import Lazy.List exposing ((:::), empty)
-import Random
-import Random.Extra
-import Random.Date
-import Shrink exposing (Shrinker)
+import Expect
+import Test exposing (describe, fuzz)
+import Fuzz
 import TestHelpers exposing (..)
 
 
 tests =
-    Check.Test.evidenceToTest <| Check.quickCheck claims
-
-
-claims : Check.Claim
-claims =
-    suite "DateFormatter"
-        [ claim "formatRange formats a single day"
-            `that` (\date -> formatRange date date)
-            `is` (\( y, m, d ) -> toString m ++ " " ++ toString d ++ ", " ++ toString y)
-            `for` { generator = randomDaTuple
-                  , shrinker = daTupleShrinker
-                  }
+    describe "DateFormatter"
+        [ fuzz daTupleFuzzer "formatRange formats a single day" <|
+            \(( y, m, d ) as date) ->
+                (formatRange date date)
+                    |> Expect.equal (toString m ++ " " ++ toString d ++ ", " ++ toString y)
         , let
-            unorderedDays =
-                (\( ( _, _, d ), d' ) -> d >= d')
+            daysWithinMonthFuzzer =
+                Fuzz.map2 (\( y, m, d ) d' -> ( y, m, d, d + d' ))
+                    daTupleFuzzer
+                    dayFuzzer
           in
-            claim "formatRange formats days within a month"
-                `that` (\( ( y, m, d ), d' ) -> formatRange ( y, m, d ) ( y, m, d' ))
-                `is` (\( ( y, m, d ), d' ) -> toString m ++ " " ++ toString d ++ "-" ++ toString d' ++ ", " ++ toString y)
-                `for` { generator =
-                            Random.map2 (,) randomDaTuple randomDay
-                                |> Random.Extra.filter (not << unorderedDays)
-                      , shrinker =
-                            Shrink.tuple ( daTupleShrinker, dayShrinker )
-                                |> Shrink.dropIf unorderedDays
-                      }
-        , let
-            unorderedMonths =
-                (\( ( _, m, _ ), m', _ ) -> monthToInt m >= monthToInt m')
-          in
-            claim "formatRange formats days between months"
-                `that` (\( ( y, m, d ), m', d' ) -> formatRange ( y, m, d ) ( y, m', d' ))
-                `is` (\( ( y, m, d ), m', d' ) -> toString m ++ " " ++ toString d ++ "-" ++ toString m' ++ " " ++ toString d' ++ ", " ++ toString y)
-                `for` { generator =
-                            Random.map3 (,,) randomDaTuple Random.Date.month randomDay
-                                |> Random.Extra.filter (not << unorderedMonths)
-                      , shrinker =
-                            Shrink.tuple3 ( daTupleShrinker, monthShrinker, dayShrinker )
-                                |> Shrink.dropIf unorderedMonths
-                      }
-        , let
-            formatDate =
-                (\( y, m, d ) -> toString m ++ " " ++ toString d ++ ", " ++ toString y)
-
-            unorderedYears =
-                (\( ( y, _, _ ), ( y', _, _ ) ) -> y >= y')
-          in
-            claim "formatRange formats days between years"
-                `that` (\( date, date' ) -> formatRange date date')
-                `is` (\( date, date' ) -> formatDate date ++ "-" ++ formatDate date')
-                `for` { generator =
-                            Random.map2 (,) randomDaTuple randomDaTuple
-                                |> Random.Extra.filter (not << unorderedYears)
-                      , shrinker =
-                            Shrink.tuple ( daTupleShrinker, daTupleShrinker )
-                                |> Shrink.dropIf unorderedYears
-                      }
-        , claim "compare returns EQ for the same date"
-            `that` (\date -> compare' date date)
-            `is` (\_ -> EQ)
-            `for` { generator = randomDaTuple
-                  , shrinker = daTupleShrinker
-                  }
-        , let
-            sameYear =
-                (\( ( y, _, _ ), ( y', _, _ ) ) -> y == y')
-          in
-            claim "compare compares year first"
-                `that` (\( date, date' ) -> compare' date date')
-                `is` (\( ( y, _, _ ), ( y', _, _ ) ) -> compare y y')
-                `for` { generator =
-                            Random.map2 (,) randomDaTuple randomDaTuple
-                                |> Random.Extra.filter (not << sameYear)
-                      , shrinker =
-                            Shrink.tuple ( daTupleShrinker, daTupleShrinker )
-                                |> Shrink.dropIf sameYear
-                      }
-        , let
-            sameMonth =
-                (\( ( _, m, _ ), m', _ ) -> monthToInt m == monthToInt m')
-          in
-            claim "compare compares month if year is the same"
-                `that` (\( ( y, m, d ), m', d' ) -> compare' ( y, m, d ) ( y, m', d' ))
-                `is` (\( ( _, m, _ ), m', _ ) -> compare (monthToInt m) (monthToInt m'))
-                `for` { generator =
-                            Random.map3 (,,) randomDaTuple Random.Date.month randomDay
-                                |> Random.Extra.filter (not << sameMonth)
-                      , shrinker =
-                            Shrink.tuple3 ( daTupleShrinker, monthShrinker, dayShrinker )
-                                |> Shrink.dropIf sameMonth
-                      }
-        , let
-            sameDay =
-                (\( ( _, _, d ), d' ) -> d == d')
-          in
-            claim "compare compares day if year and month are the same"
-                `that` (\( ( y, m, d ), d' ) -> compare' ( y, m, d ) ( y, m, d' ))
-                `is` (\( ( _, _, d ), d' ) -> compare d d')
-                `for` { generator =
-                            Random.map2 (,) randomDaTuple randomDay
-                                |> Random.Extra.filter (not << sameDay)
-                      , shrinker =
-                            Shrink.tuple ( daTupleShrinker, dayShrinker )
-                                |> Shrink.dropIf sameDay
-                      }
+            fuzz daysWithinMonthFuzzer "formatRange formats days within a month" <|
+                \( y, m, d, d' ) ->
+                    (formatRange ( y, m, d ) ( y, m, d' ))
+                        |> Expect.equal (toString m ++ " " ++ toString d ++ "-" ++ toString d' ++ ", " ++ toString y)
+          --        , let
+          --            unorderedMonths =
+          --                (\( ( _, m, _ ), m', _ ) -> monthToInt m >= monthToInt m')
+          --          in
+          --            claim "formatRange formats days between months"
+          --                `that` (\( ( y, m, d ), m', d' ) -> formatRange ( y, m, d ) ( y, m', d' ))
+          --                `is` (\( ( y, m, d ), m', d' ) -> toString m ++ " " ++ toString d ++ "-" ++ toString m' ++ " " ++ toString d' ++ ", " ++ toString y)
+          --                `for`
+          --                    { generator =
+          --                        Random.map3 (,,) randomDaTuple Random.Date.month randomDay
+          --                            |> Random.Extra.filter (not << unorderedMonths)
+          --                    , shrinker =
+          --                        Shrink.tuple3 ( daTupleShrinker, monthShrinker, dayShrinker )
+          --                            |> Shrink.dropIf unorderedMonths
+          --                    }
+          --        , let
+          --            formatDate =
+          --                (\( y, m, d ) -> toString m ++ " " ++ toString d ++ ", " ++ toString y)
+          --
+          --            unorderedYears =
+          --                (\( ( y, _, _ ), ( y', _, _ ) ) -> y >= y')
+          --          in
+          --            claim "formatRange formats days between years"
+          --                `that` (\( date, date' ) -> formatRange date date')
+          --                `is` (\( date, date' ) -> formatDate date ++ "-" ++ formatDate date')
+          --                `for`
+          --                    { generator =
+          --                        Random.map2 (,) randomDaTuple randomDaTuple
+          --                            |> Random.Extra.filter (not << unorderedYears)
+          --                    , shrinker =
+          --                        Shrink.tuple ( daTupleShrinker, daTupleShrinker )
+          --                            |> Shrink.dropIf unorderedYears
+          --                    }
+        , fuzz daTupleFuzzer "compare returns EQ for the same date" <|
+            \date ->
+                (compare' date date)
+                    |> Expect.equal EQ
+          --        , let
+          --            sameYear =
+          --                (\( ( y, _, _ ), ( y', _, _ ) ) -> y == y')
+          --          in
+          --            claim "compare compares year first"
+          --                `that` (\( date, date' ) -> compare' date date')
+          --                `is` (\( ( y, _, _ ), ( y', _, _ ) ) -> compare y y')
+          --                `for`
+          --                    { generator =
+          --                        Random.map2 (,) randomDaTuple randomDaTuple
+          --                            |> Random.Extra.filter (not << sameYear)
+          --                    , shrinker =
+          --                        Shrink.tuple ( daTupleShrinker, daTupleShrinker )
+          --                            |> Shrink.dropIf sameYear
+          --                    }
+          --        , let
+          --            sameMonth =
+          --                (\( ( _, m, _ ), m', _ ) -> monthToInt m == monthToInt m')
+          --          in
+          --            claim "compare compares month if year is the same"
+          --                `that` (\( ( y, m, d ), m', d' ) -> compare' ( y, m, d ) ( y, m', d' ))
+          --                `is` (\( ( _, m, _ ), m', _ ) -> compare (monthToInt m) (monthToInt m'))
+          --                `for`
+          --                    { generator =
+          --                        Random.map3 (,,) randomDaTuple Random.Date.month randomDay
+          --                            |> Random.Extra.filter (not << sameMonth)
+          --                    , shrinker =
+          --                        Shrink.tuple3 ( daTupleShrinker, monthShrinker, dayShrinker )
+          --                            |> Shrink.dropIf sameMonth
+          --                    }
+          --        , let
+          --            sameDay =
+          --                (\( ( _, _, d ), d' ) -> d == d')
+          --          in
+          --            claim "compare compares day if year and month are the same"
+          --                `that` (\( ( y, m, d ), d' ) -> compare' ( y, m, d ) ( y, m, d' ))
+          --                `is` (\( ( _, _, d ), d' ) -> compare d d')
+          --                `for`
+          --                    { generator =
+          --                        Random.map2 (,) randomDaTuple randomDay
+          --                            |> Random.Extra.filter (not << sameDay)
+          --                    , shrinker =
+          --                        Shrink.tuple ( daTupleShrinker, dayShrinker )
+          --                            |> Shrink.dropIf sameDay
+          --                    }
         ]
--- frontend/test/ModelTest.elm
+++ frontend/test/ModelTest.elm
@@ -1,12 +1,13 @@
 module ModelTest exposing (..)
 
-import ElmTest exposing (assertEqual, suite, test)
 import GenericSet as GSet
 import Conference
+import Expect
 import Model exposing (..)
 import Date exposing (Month(..))
 import FilteredTagSection
 import Tag exposing (Tag(..))
+import Test exposing (describe, test)
 
 
 confSet : List Conference.Model -> GSet.GenericSet Conference.Model
@@ -29,59 +30,63 @@ blankModel =
 
 
 tests =
-    suite "Model"
-        [ test "conferencesToShow filters out past events if includePastEvents is False"
-            <| let
-                blankConference =
-                    { name = ""
-                    , link = ""
-                    , startDate = ( 1, Date.Jan, 1 )
-                    , endDate = ( 1, Date.Jan, 1 )
-                    , location = ""
-                    , cfpStartDate = Nothing
-                    , cfpEndDate = Nothing
-                    , tags = []
-                    }
+    describe "Model"
+        [ test "conferencesToShow filters out past events if includePastEvents is False" <|
+            \() ->
+                let
+                    blankConference =
+                        { name = ""
+                        , link = ""
+                        , startDate = ( 1, Date.Jan, 1 )
+                        , endDate = ( 1, Date.Jan, 1 )
+                        , location = ""
+                        , cfpStartDate = Nothing
+                        , cfpEndDate = Nothing
+                        , tags = []
+                        }
 
-                conference1 =
-                    { blankConference | startDate = ( 2015, Date.Dec, 30 ), name = "a" }
+                    conference1 =
+                        { blankConference | startDate = ( 2015, Date.Dec, 30 ), name = "a" }
 
-                conference2 =
-                    { blankConference | startDate = ( 2016, Date.Jan, 2 ), name = "b" }
+                    conference2 =
+                        { blankConference | startDate = ( 2016, Date.Jan, 2 ), name = "b" }
 
-                model =
-                    { blankModel
-                        | conferences = confSet [ conference1, conference2 ]
-                        , includePastEvents = False
-                        , currentDate = ( 2016, Jan, 1 )
-                    }
-               in
-                assertEqual [ conference2 ] <| conferencesToShow model
-        , test "shouldShow does not filter out past events if includePastEvents is True"
-            <| let
-                blankConference =
-                    { name = ""
-                    , link = ""
-                    , startDate = ( 1, Date.Jan, 1 )
-                    , endDate = ( 1, Date.Jan, 1 )
-                    , location = ""
-                    , cfpStartDate = Nothing
-                    , cfpEndDate = Nothing
-                    , tags = []
-                    }
+                    model =
+                        { blankModel
+                            | conferences = confSet [ conference1, conference2 ]
+                            , includePastEvents = False
+                            , currentDate = ( 2016, Jan, 1 )
+                        }
+                in
+                    conferencesToShow model
+                        |> Expect.equal [ conference2 ]
+        , test "shouldShow does not filter out past events if includePastEvents is True" <|
+            \() ->
+                let
+                    blankConference =
+                        { name = ""
+                        , link = ""
+                        , startDate = ( 1, Date.Jan, 1 )
+                        , endDate = ( 1, Date.Jan, 1 )
+                        , location = ""
+                        , cfpStartDate = Nothing
+                        , cfpEndDate = Nothing
+                        , tags = []
+                        }
 
-                conference1 =
-                    { blankConference | startDate = ( 2015, Date.Dec, 30 ), name = "a" }
+                    conference1 =
+                        { blankConference | startDate = ( 2015, Date.Dec, 30 ), name = "a" }
 
-                conference2 =
-                    { blankConference | startDate = ( 2016, Date.Jan, 2 ), name = "b" }
+                    conference2 =
+                        { blankConference | startDate = ( 2016, Date.Jan, 2 ), name = "b" }
 
-                model =
-                    { blankModel
-                        | conferences = confSet [ conference1, conference2 ]
-                        , includePastEvents = True
-                        , currentDate = ( 2016, Jan, 1 )
-                    }
-               in
-                assertEqual [ conference1, conference2 ] <| conferencesToShow model
+                    model =
+                        { blankModel
+                            | conferences = confSet [ conference1, conference2 ]
+                            , includePastEvents = True
+                            , currentDate = ( 2016, Jan, 1 )
+                        }
+                in
+                    conferencesToShow model
+                        |> Expect.equal [ conference1, conference2 ]
         ]
