5ed3a8bd4a41723bfb647cbed65b7611cb1cd472
Rob Looby <robertjlooby@gmail.com>
2016-11-23 12:23:18 -0600

    Read port from env

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -4,11 +4,12 @@ module Main where
 import Data.Default (def)
 import Network.Wai.Middleware.RequestLogger (autoFlush, outputFormat, mkRequestLogger, OutputFormat( Apache ), IPAddrSource( FromHeader ))
 import Network.Wai.Middleware.Static (addBase, staticPolicy)
+import System.Environment (getEnv)
 import System.IO.Unsafe (unsafePerformIO)
 import Web.Scotty
 
 main :: IO ()
-main = scotty 3000 $ do
+main = scotty (read $ unsafePerformIO $ getEnv "PORT") $ do
     middleware $ unsafePerformIO $ mkRequestLogger def { outputFormat = Apache FromHeader, autoFlush = False }
     middleware $ staticPolicy (addBase "dist")
 
