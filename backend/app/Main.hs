fa350f9b1a566cd24f2a7f2b75fb2169240c3fe4
Rob Looby <robertjlooby@gmail.com>
2017-02-03 15:09:18 -0600

    Stop using IO like a jabroni

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -11,46 +11,47 @@ import Control.Monad.Trans.Control (MonadBaseControl)
 import Data.ByteString.Char8 (pack)
 import Data.Default (def)
 import Database.Persist.Class (insert, selectList)
-import Database.Persist.Postgresql (runSqlConn, SqlBackend, withPostgresqlConn)
+import Database.Persist.Postgresql (ConnectionString, runSqlConn, SqlBackend, withPostgresqlConn)
 import Database.Persist.Types (Filter)
 import Network.Wai.Middleware.RequestLogger (autoFlush, outputFormat, mkRequestLogger, OutputFormat( Apache ), IPAddrSource( FromHeader ))
 import Network.Wai.Middleware.Static (addBase, staticPolicy)
 import RunAll (migrateAll)
 import System.Environment (getEnv)
-import System.IO.Unsafe (unsafePerformIO)
 import Web.Scotty
 
+runDB :: (MonadIO m, MonadBaseControl IO m, MonadLogger m) => ConnectionString -> ReaderT SqlBackend m a -> m a
+runDB connStr query = withPostgresqlConn connStr (runSqlConn query)
+
+getPort :: IO Int
+getPort = fmap read (getEnv "PORT")
+
+getConnStr :: IO ConnectionString
+getConnStr = fmap pack $ getEnv "DATABASE_URL"
+
 main :: IO ()
-main =
-    let
-      connStr = pack $ unsafePerformIO $ getEnv "DATABASE_URL"
-      runDB :: (MonadIO m, MonadBaseControl IO m, MonadLogger m) => ReaderT SqlBackend m a -> m a
-      runDB query = withPostgresqlConn connStr (runSqlConn query)
-      getPort :: IO Int
-      getPort = fmap read (getEnv "PORT")
-    in
-      do
-        runStdoutLoggingT $ runDB migrateAll
-        port <- getPort
-        requestLogger <- mkRequestLogger def { outputFormat = Apache FromHeader, autoFlush = False }
-        scotty port $ do
-          middleware requestLogger
-          middleware $ staticPolicy (addBase "dist")
+main = do
+    connStr <- getConnStr
+    runStdoutLoggingT $ runDB connStr migrateAll
+    port <- getPort
+    requestLogger <- mkRequestLogger def { outputFormat = Apache FromHeader, autoFlush = False }
+    scotty port $ do
+        middleware requestLogger
+        middleware $ staticPolicy (addBase "dist")
 
-          get "/" $ do
-              setHeader "Content-Type" "text/html"
-              file "dist/index.html"
+        get "/" $ do
+            setHeader "Content-Type" "text/html"
+            file "dist/index.html"
 
-          get "/conferences" $ do
-              conferences <- runStdoutLoggingT $ runDB $ selectList ([] :: [Filter Conference]) []
-              json conferences
+        get "/conferences" $ do
+            conferences <- runStdoutLoggingT $ runDB connStr $ selectList ([] :: [Filter Conference]) []
+            json conferences
 
-          post "/conferences" $ do
-              let getConference :: ActionM Conference
-                  getConference = jsonData
-              conference <- getConference
-              id <- runStdoutLoggingT $ runDB $ insert conference
-              json id
+        post "/conferences" $ do
+            let getConference :: ActionM Conference
+                getConference = jsonData
+            conference <- getConference
+            id <- runStdoutLoggingT $ runDB connStr $ insert conference
+            json id
 
-          get "/health" $ do
-              text "OK"
+        get "/health" $ do
+            text "OK"
