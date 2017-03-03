5e0387a426a54173a28b3189907a0dcffb53a05d
Rob Looby <robertjlooby@gmail.com>
2017-03-03 12:56:36 -0600

    Delete old Fuzz.filter tests

--- frontend/test/ConferenceTest.elm
+++ frontend/test/ConferenceTest.elm
@@ -37,48 +37,6 @@ tests =
             \date ->
                 cfpStatus date { blankConf | cfpEndDate = Just date }
                     |> Expect.equal ( Open, Just date )
-          --        , claim "cfpStatus is (Closed, Nothing) if currentDate is after cfpEndDate"
-          --            `that` (\( currentDate, endDate ) -> cfpStatus currentDate { blankConf | cfpEndDate = Just endDate })
-          --            `is` (\_ -> ( Closed, Nothing ))
-          --            `for` { generator =
-          --                        Random.map2 (,) randomDaTuple randomDaTuple
-          --                            |> Random.Extra.filter (\( currentDate, endDate ) -> GT == DT.compare' currentDate endDate)
-          --                  , shrinker = Shrink.tuple ( daTupleShrinker, daTupleShrinker )
-          --                  }
-          --        , claim "cfpStatus is (Open, Just cfpEndDate) if currentDate is before cfpEndDate and cfpStartDate is Nothing"
-          --            `that` (\( currentDate, endDate ) -> cfpStatus currentDate { blankConf | cfpEndDate = Just endDate })
-          --            `is` (\( _, date ) -> ( Open, Just date ))
-          --            `for` { generator =
-          --                        Random.map2 (,) randomDaTuple randomDaTuple
-          --                            |> Random.Extra.filter (\( currentDate, endDate ) -> LT == DT.compare' currentDate endDate)
-          --                  , shrinker = Shrink.tuple ( daTupleShrinker, daTupleShrinker )
-          --                  }
-          --        , claim "cfpStatus is (Open, Just cfpEndDate) if currentDate is before cfpEndDate and after cfpStartDate"
-          --            `that` (\( currentDate, startDate, endDate ) -> cfpStatus currentDate { blankConf | cfpEndDate = Just endDate, cfpStartDate = Just currentDate })
-          --            `is` (\( _, _, date ) -> ( Open, Just date ))
-          --            `for` { generator =
-          --                        Random.map3 (,,) randomDaTuple randomDaTuple randomDaTuple
-          --                            |> Random.Extra.filter (\( currentDate, _, endDate ) -> LT == DT.compare' currentDate endDate)
-          --                            |> Random.Extra.filter (\( currentDate, startDate, _ ) -> GT == DT.compare' currentDate startDate)
-          --                  , shrinker = Shrink.tuple3 ( daTupleShrinker, daTupleShrinker, daTupleShrinker )
-          --                  }
-          --        , claim "cfpStatus is (Open, Just cfpEndDate) if currentDate is before cfpEndDate and equal to cfpStartDate"
-          --            `that` (\( currentDate, endDate ) -> cfpStatus currentDate { blankConf | cfpEndDate = Just endDate, cfpStartDate = Just currentDate })
-          --            `is` (\( _, date ) -> ( Open, Just date ))
-          --            `for` { generator =
-          --                        Random.map2 (,) randomDaTuple randomDaTuple
-          --                            |> Random.Extra.filter (\( currentDate, endDate ) -> LT == DT.compare' currentDate endDate)
-          --                  , shrinker = Shrink.tuple ( daTupleShrinker, daTupleShrinker )
-          --                  }
-          --        , claim "cfpStatus is (NotYetOpen, Just cfpStartDate) if currentDate is before cfpEndDate and cfpStartDate"
-          --            `that` (\( currentDate, startDate, endDate ) -> cfpStatus currentDate { blankConf | cfpEndDate = Just endDate, cfpStartDate = Just startDate })
-          --            `is` (\( _, date, _ ) -> ( NotYetOpen, Just date ))
-          --            `for` { generator =
-          --                        Random.map3 (,,) randomDaTuple randomDaTuple randomDaTuple
-          --                            |> Random.Extra.filter (\( _, startDate, endDate ) -> LT == DT.compare' startDate endDate)
-          --                            |> Random.Extra.filter (\( currentDate, startDate, _ ) -> LT == DT.compare' currentDate startDate)
-          --                  , shrinker = Shrink.tuple3 ( daTupleShrinker, daTupleShrinker, daTupleShrinker )
-          --                  }
         , fuzz2 daTupleFuzzer daTupleFuzzer "compareConferences sorts first by start date" <|
             \d1 d2 ->
                 compareConferences { blankConf | startDate = d1 } { blankConf | startDate = d2 }
--- frontend/test/DaTupleTest.elm
+++ frontend/test/DaTupleTest.elm
@@ -1,8 +1,5 @@
 module DaTupleTest exposing (..)
 
---import Date exposing (Month(..))
---import Date.Extra.Core exposing (monthToInt)
-
 import DaTuple exposing (..)
 import Expect
 import Test exposing (describe, fuzz)
@@ -26,86 +23,8 @@ tests =
                 \( y, m, d, d2 ) ->
                     (formatRange ( y, m, d ) ( y, m, d2 ))
                         |> Expect.equal (toString m ++ " " ++ toString d ++ "-" ++ toString d2 ++ ", " ++ toString y)
-          --        , let
-          --            unorderedMonths =
-          --                (\( ( _, m, _ ), m', _ ) -> monthToInt m >= monthToInt m')
-          --          in
-          --            claim "formatRange formats days between months"
-          --                `that` (\( ( y, m, d ), m', d' ) -> formatRange ( y, m, d ) ( y, m', d' ))
-          --                `is` (\( ( y, m, d ), m', d' ) -> toString m ++ " " ++ toString d ++ "-" ++ toString m' ++ " " ++ toString d' ++ ", " ++ toString y)
-          --                `for`
-          --                    { generator =
-          --                        Random.map3 (,,) randomDaTuple Random.Date.month randomDay
-          --                            |> Random.Extra.filter (not << unorderedMonths)
-          --                    , shrinker =
-          --                        Shrink.tuple3 ( daTupleShrinker, monthShrinker, dayShrinker )
-          --                            |> Shrink.dropIf unorderedMonths
-          --                    }
-          --        , let
-          --            formatDate =
-          --                (\( y, m, d ) -> toString m ++ " " ++ toString d ++ ", " ++ toString y)
-          --
-          --            unorderedYears =
-          --                (\( ( y, _, _ ), ( y', _, _ ) ) -> y >= y')
-          --          in
-          --            claim "formatRange formats days between years"
-          --                `that` (\( date, date' ) -> formatRange date date')
-          --                `is` (\( date, date' ) -> formatDate date ++ "-" ++ formatDate date')
-          --                `for`
-          --                    { generator =
-          --                        Random.map2 (,) randomDaTuple randomDaTuple
-          --                            |> Random.Extra.filter (not << unorderedYears)
-          --                    , shrinker =
-          --                        Shrink.tuple ( daTupleShrinker, daTupleShrinker )
-          --                            |> Shrink.dropIf unorderedYears
-          --                    }
         , fuzz daTupleFuzzer "compare returns EQ for the same date" <|
             \date ->
                 (compareDaTuples date date)
                     |> Expect.equal EQ
-          --        , let
-          --            sameYear =
-          --                (\( ( y, _, _ ), ( y', _, _ ) ) -> y == y')
-          --          in
-          --            claim "compare compares year first"
-          --                `that` (\( date, date' ) -> compare' date date')
-          --                `is` (\( ( y, _, _ ), ( y', _, _ ) ) -> compare y y')
-          --                `for`
-          --                    { generator =
-          --                        Random.map2 (,) randomDaTuple randomDaTuple
-          --                            |> Random.Extra.filter (not << sameYear)
-          --                    , shrinker =
-          --                        Shrink.tuple ( daTupleShrinker, daTupleShrinker )
-          --                            |> Shrink.dropIf sameYear
-          --                    }
-          --        , let
-          --            sameMonth =
-          --                (\( ( _, m, _ ), m', _ ) -> monthToInt m == monthToInt m')
-          --          in
-          --            claim "compare compares month if year is the same"
-          --                `that` (\( ( y, m, d ), m', d' ) -> compare' ( y, m, d ) ( y, m', d' ))
-          --                `is` (\( ( _, m, _ ), m', _ ) -> compare (monthToInt m) (monthToInt m'))
-          --                `for`
-          --                    { generator =
-          --                        Random.map3 (,,) randomDaTuple Random.Date.month randomDay
-          --                            |> Random.Extra.filter (not << sameMonth)
-          --                    , shrinker =
-          --                        Shrink.tuple3 ( daTupleShrinker, monthShrinker, dayShrinker )
-          --                            |> Shrink.dropIf sameMonth
-          --                    }
-          --        , let
-          --            sameDay =
-          --                (\( ( _, _, d ), d' ) -> d == d')
-          --          in
-          --            claim "compare compares day if year and month are the same"
-          --                `that` (\( ( y, m, d ), d' ) -> compare' ( y, m, d ) ( y, m, d' ))
-          --                `is` (\( ( _, _, d ), d' ) -> compare d d')
-          --                `for`
-          --                    { generator =
-          --                        Random.map2 (,) randomDaTuple randomDay
-          --                            |> Random.Extra.filter (not << sameDay)
-          --                    , shrinker =
-          --                        Shrink.tuple ( daTupleShrinker, dayShrinker )
-          --                            |> Shrink.dropIf sameDay
-          --                    }
         ]
